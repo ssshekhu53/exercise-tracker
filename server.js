b195350997b3ac3e8168bd819f8be00cf7617491
Shashank Shekhar <ssshekhu53@gmail.com>
2020-06-20 00:42:58 +0530

    Commit for heroku

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connecting to MongoDB database
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB connection established successfully');
+});
+
+const userRouter = require('./routes/users');
+const exerciseRouter = require('./routes/exercise');
+
+app.use('/user', userRouter);
+app.use('/exercise', exerciseRouter);
+
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Here we go on port: ${port}`);
+});
