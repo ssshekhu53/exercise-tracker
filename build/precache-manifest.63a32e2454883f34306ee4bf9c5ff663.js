b85d8654f839f4e5d283a63fca341880b32fa43f
Shashank Shekhar <ssshekhu53@gmail.com>
2020-06-20 00:49:57 +0530

    Added build files

---
+++ build/precache-manifest.63a32e2454883f34306ee4bf9c5ff663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7a2b8629960c988ec13dea6739bcdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee67639b5c0953934079",
+    "url": "/static/css/2.bf5b17f7.chunk.css"
+  },
+  {
+    "revision": "ee67639b5c0953934079",
+    "url": "/static/js/2.f82535a7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.f82535a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee65db5ad3bdd9084c4",
+    "url": "/static/js/main.81cc50fd.chunk.js"
+  },
+  {
+    "revision": "d8ff063ac170211915a1",
+    "url": "/static/js/runtime-main.95606020.js"
+  }
+]);
